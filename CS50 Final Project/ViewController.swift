c2796a743ccffc83896c47313b07dcb295284264
Moritz Bonacker <moritzbonacker@moritzs-macbook-pro-7.local>
2018-11-13 18:55:13 -0500

    Initial Commit

---
+++ CS50 Final Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CS50 Final Project
+//
+//  Created by Moritz Bonacker on 13.11.18.
+//  Copyright © 2018 CS50 Project Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
