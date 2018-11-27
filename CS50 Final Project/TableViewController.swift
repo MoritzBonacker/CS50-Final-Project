bddb68f6eae9b1378abf3710b62d9db8e1425406
Julian Frings <julian.frings@gmx.de>
2018-11-27 01:10:39 -0500

    TableView started

---
+++ CS50 Final Project/TableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TableViewController.swift
+//  CS50 Final Project
+//
+//  Created by Julian Frings on 27.11.18.
+//  Copyright © 2018 CS50 Project Team. All rights reserved.
+//
+
+// SEE: https://www.youtube.com/watch?v=XwXEsKRYUXU
+
+import Foundation
+import Firebase
+
+class TableViewController: UITableViewController {
+    
+    var db:Firestore!
+    
+    var sweetArray = [Charity]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        db = Firestore.firestore()
+        loadData()
+        checkForUpdates()
+        
+        
+    }
+}
