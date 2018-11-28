f32b9f4609002de5a36267eba1c25c6dd19ff164
Moritz Bonacker <moritzbonacker@moritzs-macbook-pro-7.local>
2018-11-27 22:37:56 -0500

    a

--- CS50 Final Project/Controllers/ViewController.swift
+++ CS50 Final Project/Controllers/ViewController.swift
@@ -34,6 +34,10 @@ class ViewController: UIViewController {
     }
     
     // Login functionality
+    
+    
+    
+    
     @IBAction func loginDidTouch(_ sender: AnyObject) {
         Auth.auth().signIn(withEmail: textFieldLoginEmail.text!, password: textFieldLoginPassword.text!)
         performSegue(withIdentifier: loginToContent, sender: nil)
