1afac46e0c7b41926ba24cbf550fc53e4dae08fd
Julian Frings <julian.frings@gmx.de>
2018-11-28 23:08:36 -0500

    Start Charity UI Design

---
+++ CS50 Final Project/Controllers/CharityViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CharityViewController.swift
+//  CS50 Final Project
+//
+//  Created by Julian Frings on 28.11.18.
+//  Copyright © 2018 CS50 Project Team. All rights reserved.
+//
+
+import UIKit
+
+class CharityViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var CharityPic: UIImageView!
+    @IBOutlet weak var CharityName: UILabel!
+    @IBOutlet weak var CharitySubcategory: UILabel!
+    @IBOutlet weak var DonateButton: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
