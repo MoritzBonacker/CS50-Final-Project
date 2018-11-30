5884aa1da8baf7e78f8ba77c63ee14ad11a5adde
Moritz Bonacker <moritzbonacker@moritzs-macbook-pro-7.local>
2018-11-30 00:23:43 -0500

    a

--- CS50 Final Project/Controllers/CharityViewController.swift
+++ CS50 Final Project/Controllers/CharityViewController.swift
@@ -17,15 +17,28 @@ class CharityViewController: UIViewController {
     @IBOutlet weak var CharityName: UILabel!
     @IBOutlet weak var CharitySubcategory: UILabel!
     @IBOutlet weak var DonateButton: UIButton!
-    
+    @IBOutlet weak var CharityDescription: UILabel!
+    @IBOutlet weak var CharityRating: UILabel!
+    @IBOutlet weak var CharityLink: UILabel!
     
     
     override func viewDidLoad() {
+        let chosen_category = "Health"
         let ref = Database.database().reference()
-        let query = ref.child("Charities").queryOrdered(byChild: "Category").queryEqual(toValue: "Health").observe(DataEventType.value, with: { (snapshot) in
-            let values = snapshot.value as? [String:Any]
+        let query = ref.child("Charities").queryOrdered(byChild: "Category").queryEqual(toValue: chosen_category).observe(DataEventType.value, with: { (snapshot) in
+            let values = snapshot.value as! [String : AnyObject]
+            let picture = values["American Cancer Society"]!["Picture"]
             print(values)
+            print(values["American Cancer Society"])
+            print(values["American Cancer Society"]!["Link"])
+            self.CharityName.text = values["American Cancer Society"]!["Name"] as! String
+            self.CharityLink.text = values["American Cancer Society"]!["Link"] as! String
+            self.CharityPic.image = UIImage(named: (picture as! String)) /*
+            self.CharityRating.text = values["American Cancer Society"]!["Rating"] as! String
+            self.CharityLink.text = values["American Cancer Society"]!["Link"] as! String
+            self.CharitySubcategory.text = chosen_category */
         })
+
         /*
         ref.child("Charities/Greenpeace").observeSingleEvent(of: .value) {
             (snapshot) in
