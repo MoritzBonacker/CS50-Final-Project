7cbdb568df28212dac8317ea07bd8523e11706d8
Julian Frings <julian.frings@gmx.de>
2018-11-29 22:50:33 -0500

    Donating function added

---
+++ CS50 Final Project/Controllers/DonateThanksViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DonateThanksViewController.swift
+//  CS50 Final Project
+//
+//  Created by Julian Frings on 29.11.18.
+//  Copyright © 2018 CS50 Project Team. All rights reserved.
+//
+
+import UIKit
+
+class DonateThanksViewController: UIViewController {
+
+    @IBOutlet weak var ReturnButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        ReturnButton.backgroundColor = UIColor.white
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
