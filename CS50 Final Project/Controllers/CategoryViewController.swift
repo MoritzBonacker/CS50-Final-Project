f198ce1c54fd6cf8e3edfc02046988e335a18757
Moritz Bonacker <moritzbonacker@moritzs-macbook-pro-7.local>
2018-12-05 15:34:19 -0500

    d

--- CS50 Final Project/Controllers/CategoryViewController.swift
+++ CS50 Final Project/Controllers/CategoryViewController.swift
@@ -83,33 +83,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     //var menuShowing = false
     
     override func viewDidLoad() {
-        let user = Auth.auth().currentUser
-        let ref = Database.database().reference()
-        ref.child("user").child(user!.uid).observe(DataEventType.value, with: { (snapshot) in
-            self.values = snapshot.value as? Dictionary<String, AnyObject> ?? [:]
-            self.charities = Array(self.values.keys)
-            let size = self.charities.count
-            print(self.values)
-            var names_list = [String] ()
-            var amounts_list = [Int] ()
-            for value in self.values.keys {
-                // print(self.values[value]?["Name"])
-                names_list.append(self.values[value]?["Name"] as! String)
-                // print(self.values[value]?["Amount"])
-                amounts_list.append(self.values[value]?["Amount"] as! Int)
-            }
-            print(names_list)
-            print(amounts_list)
         // Do any additional setup after loading the view, typically from a nib.
         //menuView.layer.shadowOpacity = 1
-
-        // Pushes Data to charity view controller
-        // Source: https://www.youtube.com/watch?v=7fbTHFH3tl4
-        
-        })
-
-    
-    
 }
 
 
