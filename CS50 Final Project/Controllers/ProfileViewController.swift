8f38a37f623ee67f0f4e828a2f31ffdcc519f8ec
Moritz Bonacker <moritzbonacker@moritzs-macbook-pro-7.local>
2018-12-06 14:17:58 -0500

    a

--- CS50 Final Project/Controllers/ProfileViewController.swift
+++ CS50 Final Project/Controllers/ProfileViewController.swift
@@ -1,81 +1,73 @@
-// aaa
-//  ProfileViewController.swift
-//  CS50 Final Project
-//
-//  Created by Julian Frings on 29.11.18.
-//  Copyright © 2018 CS50 Project Team. All rights reserved.
-//////
+/*  ProfileViewController.swift
+CS50 Final Project
+
+Handles the Profile View by reading data about user's transaction history and displaying it in a table view.
+The Controller also implements the functionality to log out.
+ 
+Copyright © 2018 CS50 Project Team. All rights reserved. */
 
 import UIKit
 import Firebase
 
-// https://www.youtube.com/watch?v=VFtsSEYDNRU
 class ProfileViewController: UIViewController {
     
-    var names_list = [String] ()
-    var amounts_list = [Int] ()
-    var times_list = [Double] ()
+    // Declares Dictionary to hold the data retrieved from Firebase as well as an Array in which information about past transactions will be stored
     var values: Dictionary<String, AnyObject> = [:]
+    var transactions = [Transaction] ()
     
-    
+    // Declares IBOutlets to reference particular elements in the View
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var LogoutButton: UIButton!
     @IBOutlet weak var Username: UILabel!
-    
     @IBOutlet weak var lastDonation: UILabel!
     
-    
+    // Retrieves data about user's past transactions from Firebase and stores them into an Array
     func LoadHistory() {
         
-            let user = Auth.auth().currentUser // https://firebase.google.com/docs/auth/web/manage-users
-            if user == nil {
-                print("HERE")
-                return()
-            }
-        
-            else {
-                print("UNDER THIS FUCKER")
-                print(user?.uid)
-                self.Username.text = user?.email
-                
-                let ref = Database.database().reference()
-                ref.child("user").child(user!.uid).observe(DataEventType.value, with: { (snapshot) in
-                    self.values = snapshot.value as? Dictionary<String, AnyObject> ?? [:]
-                    // print(self.values)
-                    
-                    for value in self.values.keys {
-                        // print(self.values[value]?["Name"])
-                        self.names_list.append(self.values[value]?["Name"] as! String)
-                        // print(self.values[value]?["Amount"])
-                        self.amounts_list.append(self.values[value]?["Amount"] as! Int)
-                        self.times_list.append(self.values[value]?["Timestamp"] as! Double)
-                    }
-                    let size = self.times_list.count
-                    let x = self.times_list[0] / 1000
-                    let converted = NSDate(timeIntervalSince1970: x)
-                    let formatter = DateFormatter()
-                    formatter.timeZone = NSTimeZone.local
-                    formatter.dateFormat = "MMM d, h:mm a"
-                    let time = formatter.string(from: converted as Date)
-                    print(time)
-                    self.lastDonation.text = time
-                    self.tableView.reloadData()
-                })
-                
+        // Identifies current user and only proceeds if user is logged in
+        let user = Auth.auth().currentUser // https://firebase.google.com/docs/auth/web/manage-users
+        if user == nil {
+            return()
         }
-    
+        else {
+            // Changes displayed username to user's email
+            self.Username.text = user?.email
+                
+            // Retrieves data from Firebase and stores data snapshot in Dictionary, source: https://www.youtube.com/watch?v=Aw5Hb_A_eFI
+            let ref = Database.database().reference()
+            ref.child("user").child(user!.uid).observe(.childAdded) { (snapshot: DataSnapshot) in
+            self.values = (snapshot.value as? Dictionary<String, AnyObject>)!
+
+            // Stores transaction information seperate variables
+            let transactionName = self.values["Name"] as! String
+            let transactionAmount = self.values["Amount"] as! Int
+            let transactionTime = self.values["Timestamp"] as! Double
+            
+            // Initializes an transaction instance and inserts it at the beginning of the Array. After new information is retrieved, table data is reloaded
+            let transaction = Transaction(name: transactionName, amount: transactionAmount , timestamp: transactionTime)
+            self.transactions.insert(transaction, at: 0)
+            self.tableView.reloadData()
+                    
+            // Updates time of last transaction, source: https://stackoverflow.com/questions/29243060/trying-to-convert-firebase-timestamp-to-nsdate-in-swift/44261841
+            let x = self.transactions[0].time / 1000
+            let converted = NSDate(timeIntervalSince1970: x)
+            let formatter = DateFormatter()
+            formatter.timeZone = NSTimeZone.local
+            formatter.dateFormat = "MMM d, h:mm a"
+            let time = formatter.string(from: converted as Date)
+            self.lastDonation.text = time
+                }
+        }
     }
-    //
-    //    let userID = "userID"
+    
+    // Refreshes table view with data from Firebase, source: https://www.youtube.com/watch?v=VFtsSEYDNRU as well as https://www.youtube.com/watch?v=fFpMiSsynXM and https://www.youtube.com/watch?v=uBesaTUJZi0
     override func viewDidLoad() {
         LogoutButton.roundButton()
-        
         tableView.dataSource = self
         LoadHistory()
-    // Source: https://www.youtube.com/watch?v=fFpMiSsynXM
-    // https://www.youtube.com/watch?v=uBesaTUJZi0
 }
     
+    // Implements log out functionality
     @IBAction func logoutTapped(_ sender: UIButton) {
         do {
             try Auth.auth().signOut()
@@ -84,6 +76,7 @@ class ProfileViewController: UIViewController {
         }
     }
     
+    // Creates a user alert with individualized message
     func createAlert(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
         alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (action) in
@@ -93,19 +86,19 @@ class ProfileViewController: UIViewController {
     }
 }
 
+// Provides data for table's cells
 extension ProfileViewController: UITableViewDataSource {
     
+    // Returns number of cells needed
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return (names_list.count)
+        return (transactions.count)
     }
     
+    // Changes a cell's content and returns cell
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "history", for: indexPath) as! ViewControllerTableViewCellHistory
-        
-        cell.CharityName.text = names_list[indexPath.row]
-        cell.CharityAmount.text = String(amounts_list[indexPath.row]) + " $"
-        
+        cell.CharityName.text = transactions[indexPath.row].charity_name
+        cell.CharityAmount.text = String(transactions[indexPath.row].donation_amount) + " $"
         return (cell)
     }
 }
--- CS50 Final Project/Modells/Structs.swift
+++ CS50 Final Project/Modells/Structs.swift
@@ -1,4 +1,3 @@
-/*
 //  File.swift
 //  CS50 Final Project
 //
@@ -7,67 +6,18 @@
 //
 
 import Foundation
-import Firebase
-// import Firestore
 
-// db.collection
-
-struct Charity {
-    // Test Hallo Moritz
-    let key: String
-    let name: String
-    let category: String
-    let rating: NSInteger
-    let description: String
-    // ? makes it optional for property to have a value
-    let logo: UIImage?
-    //    let ref: DatabaseReference?
+class Transaction {
+    var charity_name: String
+    var donation_amount: Int
+    var time: Double
     
-    // Initializer customizing how structs members are assigned
-    init(name: String, category: String, rating: NSInteger, description: String, key: String = "") {
-        self.key = key
-        self.name = name
-        self.category = category
-        self.rating = rating
-        self.description = description
-        self.logo = nil
-    }
-    
-    //    // Import from firebase
-    //    init(snapshot: DataSnapshot) {
-    //        key = snapshot.key
-    //        let snapshotValue = snapshot.value as! [String: AnyObject]
-    //        name = snapshotValue["name"] as! String
-    //        addedByUser = snapshotValue["addedByUser"] as! String
-    //        completed = snapshotValue["completed"] as! Bool
-    //        ref = snapshot.ref
-    //    }
-    
-    //    func toAnyObject() -> Any {
-    //        return [
-    //            "name": name,
-    //            "addedByUser": addedByUser,
-    //            "completed": completed
-    //        ]
-    //    }
-    //
-}
-
-
-struct Users {
-    
-    let key: String
-    let FirstName: String
-    let LastName: String
-    let Email: String
-    //    let ref: DatabaseReference?
-    
-    // Initializer customizing how structs members are assigned
-    init(FirstName: String, LastName: String, Email: String, key: String = "") {
-        self.key = key
-        self.FirstName = FirstName
-        self.LastName = LastName
-        self.Email = Email
+    init(name: String, amount: Int, timestamp: Double) {
+        charity_name = name
+        donation_amount = amount
+        time = timestamp
     }
 }
-*/
+
+
+
